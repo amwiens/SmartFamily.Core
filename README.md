a7c568408deab68381b79c3e142b3615df839dad
Aaron Wiens <aaron@awwebsolutions.com>
2021-03-13 21:57:52 -0600

    Added build and publish files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# SmartFamily.Gedcom
+
+A .Net Standard library for loading, saving, working with and analyzing family trees stored in the GEDCOM format.
+
+## Installation
+
+### Current build status
+
+
+### Code quality
+
+
+### Contributing
---
+++ src/SmartFamily.Core/Models/Individual.cs
@@ -0,0 +1,53 @@
+﻿using System.Numerics;
+
+namespace SmartFamily.Core.Models
+{
+    public class Individual
+    {
+        public string IndividualID { get; set; }
+
+        public string GivenName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Gender { get; set; }
+
+        //public FactDate BirthDate { get; set; }
+
+        //public FactLocation BirthLocation { get; set; }
+
+        //public FactDate DeathDate { get; set; }
+
+        //public FactLocation DeathLocation { get; set; }
+
+        public string Occupation { get; set; }
+
+        //public Age LifeSpan { get; set; }
+
+        public string Relation { get; set; }
+
+        public string RelationToRoot { get; set; }
+
+        public string Title { get; set; }
+
+        public string Suffix { get; set; }
+
+        public string Alias { get; set; }
+
+        public string FamilySearchID { get; }
+
+        public int MarriageCount { get; set; }
+
+        public int ChildrenCount { get; set; }
+
+        public string BudgieCode { get; set; }
+
+        public BigInteger Ahnentafel { get; set; }
+
+        public bool HasNotes { get; set; }
+
+        public int FactsCount { get; set; }
+
+        public int SourcesCount { get; set; }
+    }
+}
